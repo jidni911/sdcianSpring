bdacad257ce4c933b0aa1bfc545400a64c9b6434
jidni911 <gidni441@gmail.com>
2025-01-27 08:54:25 +0600

    first commit

---
+++ src/main/java/com/jidnivai/sdcian/sdcian/SdcianApplication.java
@@ -0,0 +1,14 @@
+package com.jidnivai.sdcian.sdcian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdcianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdcianApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/jidnivai/sdcian/sdcian/SdcianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jidnivai.sdcian.sdcian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdcianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
