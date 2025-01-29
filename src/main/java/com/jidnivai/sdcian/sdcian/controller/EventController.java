8859e387d89c1c9a65c92df8aa7ff324d24f1e7d
jidni911 <gidni441@gmail.com>
2025-01-29 18:52:36 +0600

    29/1

--- src/main/java/com/jidnivai/sdcian/sdcian/controller/EventController.java
+++ src/main/java/com/jidnivai/sdcian/sdcian/controller/EventController.java
@@ -3,6 +3,7 @@ package com.jidnivai.sdcian.sdcian.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.jidnivai.sdcian.sdcian.interfaces.EventServiceInt;
 
 @RestController
 @RequestMapping("/event")
+@CrossOrigin(origins = "http://localhost:4200")
 public class EventController {
 
     @Autowired
