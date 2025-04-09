84dcd312de034c6102fb72956fc52c3f06defa2a
jidni911 <gidni441@gmail.com>
2025-04-09 12:19:48 +0600

    perfecter upload configs

--- src/main/java/com/jidnivai/sdcian/sdcian/config/WebConfig.java
+++ src/main/java/com/jidnivai/sdcian/sdcian/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.jidnivai.sdcian.sdcian.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,11 +8,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${sdcian.app.images-upload-path}")
+    private String imageFolder;
+
+    @Value("${sdcian.app.videos-upload-path}")
+    private String videoFolder;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/images/**").addResourceLocations("file:uploads/images/");
+        // Using the injected paths from application.properties
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:" + imageFolder);
 
-        registry.addResourceHandler("/videos/**").addResourceLocations("file:uploads/videos/");
+        registry.addResourceHandler("/videos/**")
+                .addResourceLocations("file:" + videoFolder);
     }
-
 }
